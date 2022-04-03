02a4870b90c33a58cf636ae93787b7db19346744
ilkhomumaraliev <88801014+ilkhomumaraliev@users.noreply.github.com>
2022-04-04 00:17:50 +0500

    Add files via upload

---
+++ Quiz App/script.js
@@ -0,0 +1,75 @@
+const quizData = [
+  {
+    question: `Xato qilmoq?`,
+    a: "Do mistake",
+    b: "Get mistake",
+    c: "Make mistake",
+    d: "Take mistake",
+    correct: "c",
+  },
+  {
+    question: "Pul ishlamoq?",
+    a: "Take money",
+    b: "Make money",
+    c: "Get Money",
+    d: "Hate money",
+    correct: "b",
+  },
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deselectAnswers();
+  const currentQuizData = quizData[currentQuiz];
+  questionEl.innerText = currentQuizData.question;
+  a_text.innerText = currentQuizData.a;
+  b_text.innerText = currentQuizData.b;
+  c_text.innerText = currentQuizData.c;
+  d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
+}
+
+function getSelected() {
+  let answer;
+
+  answerEls.forEach((answerEl) => {
+    if (answerEl.checked) {
+      answer = answerEl.id;
+    }
+  });
+
+  return answer;
+}
+
+submitBtn.addEventListener("click", () => {
+  const answer = getSelected();
+  if(answer) {
+      if(answer === quizData[currentQuiz].correct) {
+          score++;
+      }
+
+      currentQuiz++;
+      if(currentQuiz < quizData.length) {
+          loadQuiz();
+      } else {
+           quiz.innerHTML = `<h2> Siz ${quizData.length} savoldan ${score} to'g'ri topdingiz 😜 </h2>
+           <button onclick="location.reload()">Qaytadan Boshlash</button>`
+      }
+  }
+});
